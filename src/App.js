4233e673909faf347065fa40bb007f3199928b0e
Esenov-Kubanychbek <kubaesenov031005@gmail.com>
2021-05-07 14:29:25 +0600

    addded componenets orderSummury

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import Layout from "./components/Layout/Layout";
-import Menu from "./components/Menu/Menu"
-
-import "./App.css";
-import { Route, Switch } from "react-router";
+import Menu from "./components/Menu/Menu";
 import Checkout from "./components/Checkout/Checkout";
 import Orders from "./components/Orders/Orders";
 
+import "./App.css";
+import { Redirect, Route, Switch } from "react-router";
 const App = () => {
   return (
     <div className="App">
       <Layout>
         <Switch>
-          <Route path="/" component={Menu} exact/>
-          <Route path="/checkout" component={Checkout}/>
-          <Orders path="/checkout" component={Checkout}/>
-
+          <Route path="/" component={Menu} exact />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Redirect to="/" />
         </Switch>
       </Layout>
     </div>
   );
 }
-
 export default App;
