2583c58518be59adee98fd2430c00ce77a88f3bf
Esenov-Kubanychbek <kubaesenov031005@gmail.com>
2021-05-19 16:29:48 +0600

    Added placeholder in Checkout from

--- src/components/Checkout/CheckoutForm/CheckoutForm.js
+++ src/components/Checkout/CheckoutForm/CheckoutForm.js
@@ -6,15 +6,15 @@ const CheckoutForm = ({ cancelCallback, submitCallback }) => {
     <form className={classes.CheckoutForm} onSubmit={submitCallback}>
       <div>
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" required />
+        <input type="text" name="name" id="name" placeholder="Kubanychbek" required />
       </div>
       <div>
         <label htmlFor="address">Address</label>
-        <input type="text" name="address" id="address" required />
+        <input type="text" name="address" id="address" placeholder="Kyrgyztan" required />
       </div>
       <div>
         <label htmlFor="phone">Phone</label>
-        <input type="text" name="phone" id="phone" required pattern="0[0-9]{9}" />
+        <input type="text" name="phone" id="phone" placeholder="+996704447955" required pattern="0[0-9]{9}" />
       </div>
       <Button>Checkout</Button>
       <Button onClick={cancelCallback}>Cancel</Button>
