64a02a104df5d366b4f6988dc906a9db43126c1d
Esenov-Kubanychbek <kubaesenov031005@gmail.com>
2021-05-25 14:41:49 +0600

    change in App .js added useEffect

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import Menu from "./components/Menu/Menu";
 import Checkout from "./components/Checkout/Checkout";
 import Orders from "./components/Orders/Orders";
 
+import { restore } from "./store/actions/auth";
+
 import "./App.css";
 import { Redirect, Route, Switch } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const App = () => {
+  const disptach = useDispatch();
+  const isAuthenticated = useSelector(state => state.auth.token !== null);
+
+  useEffect(() => {
+    disptach(restore());
+  }, [disptach]);
+
+
   return (
     <div className="App">
       <Layout>
